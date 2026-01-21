3823a0e15a1c0577657eb3a334d4405ece5df028
beatrizbranfb <beatrizbfb@icloud.com>
2026-01-21 15:41:03 -0300

    conexão inicial

---
+++ Ferramentas com OpenAI/1_langchainopenAI.py
@@ -0,0 +1,24 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+import os   
+
+load_dotenv()
+api_key = os.getenv("OpenAi_API_Key")
+
+numero_dias = 7
+numero_adultos = 2  
+numero_criancas = 2
+cidade_destino = "Salvador"
+mes_viagem = "Julho"
+
+prompt = f"Crie um roteiro de viagem de {numero_dias} dias, para uma família com {numero_adultos} adultos e {numero_criancas} crianças, visitando {cidade_destino} durante o mês de {mes_viagem}"
+
+cliente = OpenAI(api_key=api_key)
+
+resposta = cliente.chat.completions.create(
+    model="gpt-5.2",
+    messages=[
+        {"role": "system", "content": "Você é um assistente de planejamento de viagens."},
+        {"role": "user", "content": prompt}
+    ]
+)
--- Ferramentas com OpenAI/conexao.py
+++ Ferramentas com OpenAI/conexao.py
@@ -1,6 +1,7 @@
-from openai import OpenAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 api_key = os.getenv("OpenAi_API_Key")
@@ -11,18 +12,26 @@ numero_criancas = 2
 cidade_destino = "Salvador"
 mes_viagem = "Julho"
 
-
-prompt = f"Crie um roteiro de viagem de {numero_dias} dias, para uma família com {numero_adultos} adultos e {numero_criancas} crianças, visitando {cidade_destino} durante o mês de {mes_viagem}"
-
-cliente = OpenAI(api_key=api_key)
-
-resposta = cliente.chat.completions.create(
-    model="gpt-5.2",
-    messages=[
-        {"role": "system", "content": "Você é um assistente de planejamento de viagens."},
-        {"role": "user", "content": prompt}
-    ]
+modelo_prompt = PromptTemplate(
+    template="""
+Crie um roteiro de viagem de {numero_dias} dias, 
+para uma família com {numero_adultos} adultos e {numero_criancas} crianças, 
+visitando {cidade_destino},
+ durante o mês de {mes_viagem}
+ """
+)
+prompt = modelo_prompt.format(
+    numero_dias=numero_dias,    
+    numero_adultos=numero_adultos,
+    numero_criancas=numero_criancas,
+    cidade_destino=cidade_destino,
+    mes_viagem=mes_viagem
+)
+modelo = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.5,
+    api_key=api_key
 )
 
-resposta_texto = resposta.choices[0].message.content
-print(resposta_texto)
+resposta = modelo.invoke(prompt)
+print(resposta.content)
